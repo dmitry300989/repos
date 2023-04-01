2088005c00b635790faf3d61b3202806a17c23d6
dmitry300989 <antonpovi@yandex.ru>
2023-04-01 18:52:24 +0200

    Fix

---
+++ Codeforces/732A_buy_a_shovel/732A_buy_a_shovel.cpp
@@ -0,0 +1,26 @@
+﻿// 732A_buy_a_shovel.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int k = 0, r = 0;
+	cin >> k;
+	cin >> r;
+	int res = 0;
+	for (int i = 1; i <= 10; ++i)
+	{
+		int t = ((k % 10) * i) % 10;
+		if (t == r || t == 0)
+		{
+			res = i;
+			break;
+		}
+	}
+	cout << res << endl;
+	return 0;
+}
