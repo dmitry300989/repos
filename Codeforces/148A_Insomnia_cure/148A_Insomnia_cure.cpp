419d8583374dbd3c26f5a1b2586044503ade490c
dmitry300989 <antonpovi@yandex.ru>
2023-04-01 14:40:53 +0200

    Insomnia cure - init commit

---
+++ Codeforces/148A_Insomnia_cure/148A_Insomnia_cure.cpp
@@ -0,0 +1,9 @@
+﻿// 148A_Insomnia_cure.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
